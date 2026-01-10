04ece9e8026f5ad8110f1c3febca86b1f70516d3
AnithaAnnem <anitha.annem.snaatak@mygurukulam.co>
2026-01-10 14:05:26 +0530

    Update render-manifest.sh

--- scripts/render-manifest.sh
+++ scripts/render-manifest.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 set -e
 
+ENV=$1
+
 echo "Starting render step"
 pwd
 ls -la
 
-echo "Rendering kustomize output"
-kustomize build overlay/dev > final-manifest.yaml
+APP_NAME=$(yq e '.appName' parameter.yml)
+IMAGE=$(yq e '.image.repo' parameter.yml)
+TAG=$(yq e '.image.tag' parameter.yml)
+REPLICAS=$(yq e ".replicas.${ENV}" parameter.yml)
 
-echo "Manifest rendered successfully"
-ls -la final-manifest.yaml
+echo "APP_NAME=$APP_NAME"
+echo "IMAGE=$IMAGE"
+echo "TAG=$TAG"
+echo "REPLICAS=$REPLICAS"
 
+echo "Replacing placeholders..."
+
+find base overlay -type f -name "*.yaml" -exec sed -i \
+  -e "s/APP_NAME/${APP_NAME}/g" \
+  -e "s|IMAGE|${IMAGE}|g" \
+  -e "s/TAG/${TAG}/g" \
+  -e "s/REPLICAS/${REPLICAS}/g" {} +
+
+echo "Render completed successfully"
 exit 0
